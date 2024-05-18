fb32c0c65168bd34d25e8675b2eae857b118a6e0
NayJi7 <NayJi7@github.com>
2024-05-18 12:35:07 +0200

    base

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Cy-Trip </title>
+    <link rel="icon" href="source/plane.png">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link rel="stylesheet" type="text/css" href="style/footer.css">
+    <script type="text/javascript" src="script/main.js"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <!-- espace pour link des fonts qu'on voudrait ajouter au site -->
+</head>
+
+<body>
+    <header>
+
+    </header>
+
+    <main>
+        <div class="map">
+            <img id="worldmap" src="source/worldmap.jpg" alt="worldmap">
+            <a href="">
+                <div class="france"></div>
+            </a>
+        </div>
+    </main>
+
+    <?php require_once(__DIR__ . '/footer.php'); ?>
+</body>
+
+</html>
