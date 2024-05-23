e1f11c3db6f8772b7ce284ab38e8d714733cbade
Terrak Adam <terrakadam@prc-cy309-009.pedagogie.cyu.fr>
2024-05-23 09:57:23 +0200

    links

--- php/footer.php
+++ php/footer.php
@@ -2,10 +2,10 @@
     <a href="/index.php" id="firsta"><img id="logofooter" src="/source/icon.png" alt="icon"></a>
 
     <div class="names">
-        <button id="nametext" type="us">Louaye Saghir</button>
-        <button id="nametext" type="us">Abdellah El Harsal</button>
-        <button id="nametext" type="us">Adam Terrak</button>
-        <button id="nametext" type="us">Ayman Ouguerd</button>
+        <a href="mailto:saghirloua@cy-tech.fr" id="nametext" type="us">Louaye Saghir</a>
+        <a href="mailto:abdelah.elharsal@gmail.com" id="nametext" type="us">Abdelah El Harsal</a>
+        <a href="mailto:e.aterrak@gmail.com" id="nametext" type="us">Adam Terrak</a>
+        <a href="mailto:aymanouguerd@gmail.com" id="nametext" type="us">Ayman Ouguerd</a>
     </div>
 
     <a href="https://cytech.cyu.fr"><img id="cylogo" src="/source/cytech.png" alt=""></a>
