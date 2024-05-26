8c5537410c783462099c2617fac29a84e1bd61f3
NayJi7 <NayJi7@github.com>
2024-05-27 01:35:02 +0200

    final final push

--- php/sqlconfig.php
+++ php/sqlconfig.php
@@ -1,13 +1,10 @@
 <?php 
-    // Connexion a la base de donnees MySQL
     try
     {
         $mysqlClient = new PDO('mysql:host=127.0.0.1;dbname=cytrip;charset=utf8', 'root', '');
     }
-    // Gestion des erreurs en cas d'echec de la connexion
     catch (Exception $e)
     {    
-        // Affichage de l'erreur et arrêt de l'exécution du script
         die('Erreur : ' . $e->getMessage());
     }
 ?>
